7b30904f001b222114f2a4fa2a7d1b201becca84
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-04-12 01:36:17 -0500

    Finished really bad website

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const h1 = document.createElement('h1');
-h1.textContent = "Welcome to an Informative Website about Me!"
-document.querySelector('body').appendChild(h1);
-
 const h6 = document.createElement('h6');
 h6.textContent = "(Trying to get used to JavaScript, but it's hard D:)"
 document.querySelector('body').appendChild(h6);
+
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("demo").style.color = "blue";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+}
