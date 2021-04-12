57a5020a7fc528557441b8b8497ff278b9e5aacb
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-04-12 00:20:01 -0500

    Finished really bad website

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.createElement('h1');
+h1.textContent = "Welcome to an Informative Website about Me!"
+document.querySelector('body').appendChild(h1);
+
+const h6 = document.createElement('h6');
+h6.textContent = "(Trying to get used to JavaScript, but it's hard D:)"
+document.querySelector('body').appendChild(h6);
